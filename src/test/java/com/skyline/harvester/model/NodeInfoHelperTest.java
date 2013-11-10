0810a877b4ccf78b667505a51f598f6aed8f4619
b0noI <viacheslav@b0noi.com>
2013-11-10 00:03:29 +0000

    adding tests

---
+++ src/test/java/com/skyline/harvester/model/NodeInfoHelperTest.java
@@ -0,0 +1,11 @@
+package com.skyline.harvester.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: b0noi
+ * Date: 11/9/13
+ * Time: 11:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NodeInfoHelperTest {
+}
---
+++ src/test/java/com/skyline/harvester/server/ServerTest.java
@@ -0,0 +1,11 @@
+package com.skyline.harvester.server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: b0noi
+ * Date: 11/9/13
+ * Time: 11:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerTest {
+}
